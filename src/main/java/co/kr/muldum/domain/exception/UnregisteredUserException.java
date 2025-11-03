f99414196092a17b2982ed5131baae8a5f70315c
randirao <randirao4245@gmail.com>
2025-11-04 08:28:40 +0900

    feat: 학생 학번 정보를 포함한 커스텀 예외 팩토리 추가

--- src/main/java/co/kr/muldum/domain/exception/UnregisteredUserException.java
+++ src/main/java/co/kr/muldum/domain/exception/UnregisteredUserException.java
@@ -8,6 +8,11 @@ public class UnregisteredUserException extends BusinessException {
     private static final HttpStatus HTTP_STATUS = HttpStatus.CONFLICT;
 
     public UnregisteredUserException(String identifier) {
-        super("등록되지 않은 사용자입니다: " + identifier, HTTP_STATUS, ERROR_CODE);
+        super("등록되지 않은 사용자입니다. 확인이 필요한 식별자: " + identifier, HTTP_STATUS, ERROR_CODE);
+    }
+
+    public static UnregisteredUserException forStudent(int grade, int classNo, int studentNo, int rowNumber) {
+        String identifier = String.format("학년 %d, 반 %d, 번호 %d (시트 행 %d)", grade, classNo, studentNo, rowNumber);
+        return new UnregisteredUserException(identifier);
     }
 }
