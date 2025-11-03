bc00f4143a1b197578dcb97ed63ae759fd5f0a17
randirao <randirao4245@gmail.com>
2025-11-04 08:16:36 +0900

    feat: 팀 저장 및 멤버 교체 포트 정의

---
+++ src/main/java/co/kr/muldum/application/port/out/TeamManagementPort.java
@@ -0,0 +1,17 @@
+package co.kr.muldum.application.port.out;
+
+import co.kr.muldum.domain.model.Member;
+import co.kr.muldum.domain.model.Team;
+import co.kr.muldum.domain.model.TeamType;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface TeamManagementPort {
+
+    Optional<Team> findByNameAndType(String name, TeamType type);
+
+    Team saveTeam(Team team);
+
+    void replaceMembers(Long teamId, List<Member> members);
+}
