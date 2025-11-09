fa4105caa50be8208e1ee2fcab89d206630892c1
randirao <randirao4245@gmail.com>
2025-11-10 01:36:12 +0900

    fix: 로그인관련 로직 삭제

--- src/main/java/co/kr/muldum/application/port/in/GoogleLoginUseCase.java
+++
@@ -1,8 +0,0 @@
-package co.kr.muldum.application.port.in;
-
-import co.kr.muldum.application.port.in.command.GoogleLoginCommand;
-import co.kr.muldum.application.port.in.response.LoginResponse;
-
-public interface GoogleLoginUseCase {
-    LoginResponse login(GoogleLoginCommand command);
-}
--- src/main/java/co/kr/muldum/application/port/out/GoogleOAuthPort.java
+++
@@ -1,5 +0,0 @@
-package co.kr.muldum.application.port.out;
-
-public interface GoogleOAuthPort {
-    String getEmailFromAuthCode(String authorizationCode);
-}
